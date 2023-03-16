e8043402c3194238df0de48e8e24114756e8a756
Sai Ranga Pavan <121741348+Bathalapalli-SaiRangaPavan@users.noreply.github.com>
2023-03-16 15:05:10 +0530

    updated BuildServer IP

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://44.199.250.205:9999/'+e.target.name;
+    const url = 'http://100.25.193.105:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
